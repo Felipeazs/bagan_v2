1abf5efb1d106d65cbba2ce12cc9277850b32ebe
felipeazs <felipeazs@gmail.com>
2024-10-13 20:02:34 -0300

    not found page

--- views/components/NotFound.tsx
+++ views/components/NotFound.tsx
@@ -1,10 +1,15 @@
-import { Link } from "@tanstack/react-router"
+import { useNavigate } from "@tanstack/react-router"
+import { Button } from "./ui/button"
 
 const NotFound = () => {
+	const navigate = useNavigate()
+
 	return (
-		<div>
-			<p>Not Found!</p>
-			<Link to="/">Volver</Link>
+		<div className="flex flex-col gap-5 justify-center items-center h-screen text-black">
+			<h1 className="text-4xl">Página no encontrada!</h1>
+			<Button type="button" onClick={() => navigate({ to: "/" })}>
+				Volver
+			</Button>
 		</div>
 	)
 }
