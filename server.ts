c8521a83bae7c4318fe116301574d100a8f23cd8
felipeazs <felipeazs@gmail.com>
2024-10-16 15:43:41 -0300

    origin

--- server.ts
+++ server.ts
@@ -39,7 +39,7 @@ process.on("uncaughtException", (err) => {
 const app = new Hono()
 app.use(
 	cors({
-		origin: ["https://*.bagan.cl", "https://strapi_dev.bagan.cl"],
+		origin: process.env["MP_REDIRECT"]!,
 	}),
 )
 app.use(logger())
